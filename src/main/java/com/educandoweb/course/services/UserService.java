ecd6b549e8d8e8b2ebf4b9a4122af3fabbc3a3ce
brunokudaka <brunokudaka91@gmail.com>
2025-02-15 09:42:14 -0300

    User update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -31,4 +31,16 @@ public class UserService {
         repository.deleteById(id);
     }
 
+    public User update(Long id, User user){
+        User entity = repository.getReferenceById(id);
+        updteData(entity, user);
+        return repository.save(entity);
+    }
+
+    private void updteData(User entity, User user) {
+        entity.setName(user.getName());
+        entity.setEmail(user.getEmail());
+        entity.setPhone(user.getPhone());
+    }
+
 }
